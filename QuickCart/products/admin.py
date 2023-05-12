d115ded3cce0ecd08600feca9cc608018a4e4c21
Shrey <shreyshukla1010@gmail.com>
2023-05-13 00:48:34 +0530

    added color size and size option for each product

--- QuickCart/products/admin.py
+++ QuickCart/products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(product),
-admin.site.register(Category),
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(ColorVariant)
+admin.site.register(SizeVariant)
+admin.site.register(QuantityVariant)
